7ae3b250974dbdd2b581a5355f2e8890b9e29f8b
jordane <jordane.cure@gmail.com>
2019-08-06 21:59:14 -0400

    Set menu style for mobile

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, QueryList, ViewChildren, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { GameOfLifeService } from 'src/app/services/game-of-life.service';
 import { Subscription } from 'rxjs';
 import { Universe } from 'src/app/models/Universe';
@@ -9,12 +9,33 @@ import { ExportedCells } from 'src/app/models/ExportedCells';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit, OnDestroy {
+export class MenuComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  @ViewChildren('container') containers !: QueryList<ElementRef>;
+  @ViewChild('menu', { static: false }) menu !: ElementRef;
 
   public universe: Universe;
 
   private subscriptions: Subscription[] = [];
   public universeState: 'started' | 'stoped';
+
+  // tslint:disable-next-line: variable-name
+  private _menuLevel = 0;
+
+  public get visibleMenuLevel(): number {
+    return this._menuLevel;
+  }
+
+  public set visibleMenuLevel(visibleMenuLevel: number) {
+    let heightToShow = 0;
+    this.containers.forEach((elementRef, menuLevel) => {
+      if (menuLevel !== 0 && menuLevel <= visibleMenuLevel) {
+        heightToShow += elementRef.nativeElement.clientHeight;
+      }
+    });
+    this.menu.nativeElement.style.top = -heightToShow + 'px';
+    this._menuLevel = visibleMenuLevel;
+  }
 
   // TODO: Put that away and rename it
   public defaultCells: { viewValue: string, cells: ExportedCells }[] = [
@@ -27,8 +48,6 @@ export class MenuComponent implements OnInit, OnDestroy {
   ];
 
   public selectedDefault = this.defaultCells[0];
-
-
 
   constructor(public gameOfLife: GameOfLifeService) { }
 
@@ -44,6 +63,10 @@ export class MenuComponent implements OnInit, OnDestroy {
         this.universeState = universeState;
       })
     );
+  }
+
+  ngAfterViewInit() {
+    this.visibleMenuLevel = 0;
   }
 
   ngOnDestroy() {
